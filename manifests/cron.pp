b1a47a44319c9f27d0ef6ccb8caab952b132bb9b
Robert Hafner <tedivm@tedivm.com>
2015-06-08 14:35:54 -0700

    Added missing commas

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -11,7 +11,7 @@ class ghebackups::cron (
   $restore_exec = "${install_location}/bin/ghe-restore -c -v 1>>${log_location}/${log_restore}"
 
   cron { 'ghe_backups':
-    command => $backup_exec
+    command => $backup_exec,
     user    => root,
     hour    => $cron_hour,
     minute  => 0
@@ -19,7 +19,7 @@ class ghebackups::cron (
 
   if $restore {
     cron { 'ghe_restore':
-      command => $restore_exec
+      command => $restore_exec,
       user    => root,
       hour    => $cron_hour,
       minute  => 30
